9a009a9b2bed3bcc3b3e2dea685c06fac455f189
Daniel <dtch1997@stanford.edu>
2021-04-18 14:48:12 +0800

    Fix solve(); fix variables and parameters

--- src/scp_solver.py
+++ src/scp_solver.py
@@ -2,9 +2,11 @@ import cvxpy as cp
 import numpy as np 
 from typing import Dict
 
-def curr(var: cp.Variable):
+
+""" Slicing convenience functions """
+def nxt(var: cp.Variable):
     return var[1:]
-def prev(var: cp.Variable):
+def curr(var: cp.Variable):
     return var[:-1]
 
 class AttrDict(Dict):
@@ -23,18 +25,22 @@ class SCPSolver:
     def __init__(self, num_time_steps, duration):
         self.num_time_steps = num_time_steps
         self.duration = duration
-        self.time_step_magnitude = duration / num_time_steps
+        self.time_step_magnitude = duration / num_time_steps 
+
         # Parameter semantics: 
         #   State variables:
         #   - self.variables.xpos[0] is the initial state
         #   Inputs:
         #   - state[t+1] = f(state[t], input[t])
+        zeros = np.zeros(num_time_steps+1)
         self.parameters = AttrDict.from_dict({
-            "prev_xpos": cp.Parameter(shape = num_time_steps+1),
-            "prev_ypos": cp.Parameter(shape = num_time_steps+1),
-            "prev_velocity": cp.Parameter(shape = num_time_steps+1),
-            "prev_theta": cp.Parameter(shape = num_time_steps+1),
-            "prev_kappa": cp.Parameter(shape = num_time_steps+1)
+            "prev_xpos": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_ypos": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_velocity": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_theta": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_kappa": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_acceleraton": cp.Parameter(shape = num_time_steps+1, value = zeros.copy()),
+            "prev_juke": cp.Parameter(shape = num_time_steps+1, value = zeros.copy())
         })
         self.variables = AttrDict.from_dict({
             "xpos": cp.Variable(num_time_steps+1),
@@ -42,8 +48,10 @@ class SCPSolver:
             "velocity" : cp.Variable(num_time_steps+1),
             "theta" : cp.Variable(num_time_steps+1),
             "kappa" : cp.Variable(num_time_steps+1),
+            "acceleration": cp.Variable(num_time_steps+1), 
+            "pinch" : cp.Variable(num_time_steps+1),
             "jerk" : cp.Variable(num_time_steps),
-            "pinch" : cp.Variable(num_time_steps)
+            "juke" : cp.Variable(num_time_steps),
         })
 
         self.problem = cp.Problem(self.objective, self.constraints)
@@ -77,19 +85,47 @@ class SCPSolver:
 
     @property
     def constraints(self):        
-        # Previous state trajectories are stored in self.parameters
+        xpos = self.variables.xpos 
+        ypos = self.variables.ypos
+        veloc = self.variables.velocity
+        theta = self.variables.theta
+        kappa = self.variables.kappa 
+        jerk = self.variables.jerk
+        pinch = self.variables.pinch
+
+        # Previous estimates of state trajectories are stored in self.parameters
+        prev_xpos = self.parameters.prev_xpos
+        prev_ypos = self.parameters.prev_ypos 
+        prev_veloc = self.parameters.prev_velocity
+        prev_theta = self.parameters.prev_theta
+        prev_kappa = self.parameters.prev_kappa
+
+        h = self.num_time_steps 
+        delta_theta = curr(theta) - curr(prev_theta) # 0i - 0i^{(k)}
+
         return [
-            # This is just here for debugging right now
-            cp.sum(self.variables.xpos) == 1
+            nxt(xpos) == curr(xpos) + h * (
+                    cp.multiply(curr(veloc), np.cos(curr(prev_theta).value)) 
+                    - cp.multiply(cp.multiply(curr(prev_veloc), np.sin(curr(prev_theta).value)), delta_theta)
+                ),
+            nxt(ypos) == curr(ypos) + h * (
+                    cp.multiply(curr(veloc), np.sin(curr(prev_theta).value)) 
+                    - cp.multiply(cp.multiply(curr(prev_veloc), np.cos(curr(prev_theta).value)), delta_theta)
+                ),
+            nxt(theta) == curr(theta) + h * (
+                    cp.multiply(curr(veloc), curr(prev_kappa.value))
+                    + cp_multiply(curr(prev_veloc), curr(kappa) - curr(prev_kappa)))
+                )
         ]
 
-    def solve(self):
-        self.problem.solve()
+    def solve(self) -> float:
+        optval = self.problem.solve()
         # Initialize the parameters to the values found by solve
         # This way, the next time we call 'solve' we'll already have the right values
         for param_key in self.parameters.keys():
             var_key = param_key[5:] # get the corresponding variable
             self.parameters[param_key].value = self.variables[var_key].value
+        return optval
 
 if __name__ == "__main__":
     solver = SCPSolver(100, 10)
