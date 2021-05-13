5b4f2caf4dd4d7acfaee85162a8d3b11f98e46f5
Daniel Tan <dtch1997@users.noreply.github.com>
2021-05-13 13:12:58 +0800

    Add training runner skeleton

--- src/runners/train.py
+++ src/runners/train.py
@@ -1,6 +1,22 @@
 class TrainingRunner:
+
     def __init__(self):
         pass
 
+    def __init__(self, args, env, agent):
+        self.env = env
+        self.agent = agent
+
+    @staticmethod 
+    def add_argparse_args(parser):
+        return parser 
+
+    @staticmethod 
+    def from_argparse_args(args, env, agent):
+        return TrainingRunner(args, env, agent)
+
+    def run(self):
+        raise NotImplementedError
     
+
     
