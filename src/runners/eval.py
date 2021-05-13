477a3a459e0e9a85bd8cbf3bb1a4039323caaf99
prestonarogers <parogers@stanford.edu>
2021-05-13 15:00:04 -0700

    Added changes for data saving
    Modified train.py to train based on saved data from eval.py

--- src/runners/eval.py
+++ src/runners/eval.py
@@ -11,10 +11,12 @@ class EvaluationRunner:
         self.env = env
         self.agent = agent
         self.num_simulation_time_steps = args.num_simulation_time_steps
+        self.saved_filepath = args.save_filepath
 
     @staticmethod 
     def add_argparse_args(parser):
         parser.add_argument("--num-simulation-time-steps", type=int, default=1200)
+        parser.add_argument("--save-filepath", default="saved_data.csv")
         return parser 
 
     @staticmethod 
@@ -25,18 +27,25 @@ class EvaluationRunner:
         self.env.reset() 
         self.agent.reset(self.env)
 
-        actual_trajectory = np.zeros((self.num_simulation_time_steps + 1, 7))
+        actual_trajectory = np.zeros((self.num_simulation_time_steps + 1, self.agent.num_states))
         initial_state = self.env.current_state
         current_state = self.env.current_state
         actual_trajectory[0] = current_state
 
+        data =  np.zeros((1, self.agent.num_states+self.agent.num_actions))
+
         for i in range(self.num_simulation_time_steps):
             self.env.render()
             action = self.agent.get_action(current_state)
+            if i == 0:
+                data = np.hstack(current_state, action)
+            else:
+                data = np.vstack(data, np.hstack(current_state, action))
             next_state, reward, done, info = self.env.take_action(action)            
             current_state = next_state
             actual_trajectory[i+1] = current_state
 
+        np.savetxt("saved_data.csv", data, delimiter=",")
         OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
         plot_trajectory(initial_state, self.env.goal_state, actual_trajectory, str(OUTPUT_DIR / 'actual_trajectory.png'))
         return actual_trajectory
--- src/runners/train.py
+++ src/runners/train.py
@@ -1,22 +1,75 @@
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+
+class CarDataset(Dataset):
+    def __init__(self, data_filepath, transform=None, target_transform=None):
+        data = np.loadtxt(open(data_filepath), delimiter=",")
+        self.state_data = data[:,:-3]
+        self.action_data = data[:,-3:]
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return self.state_data.shape[0]
+
+    def __getitem__(self, idx):
+        state = torch.from_numpy(self.state_data[idx,:])
+        action = torch.from_numpy(self.action_data[idx,:])
+        if self.transform:
+            state = self.transform(state)
+        if self.target_transform:
+            action = self.target_transform(action)
+        sample = {"state": state, "action": action}
+        return sample
+
 class TrainingRunner:
-
-    def __init__(self):
-        pass
-
     def __init__(self, args, env, agent):
         self.env = env
         self.agent = agent
+        self.learning_rate = args.learning_rate
+        self.batch_size = args.batch_size
+        self.epochs = args.epochs
 
     @staticmethod 
     def add_argparse_args(parser):
+        parser.add_argument('--learning-rate', default = None)
+        parser.add_argument('--batch-size', default = None)
+        parser.add_argument('--epochs', default = None)
+        parser.add_argument('--data-filepath', default = None)
         return parser 
 
     @staticmethod 
     def from_argparse_args(args, env, agent):
         return TrainingRunner(args, env, agent)
+    
+    def train_loop(self, dataloader, loss_fn, optimizer):
+        size = len(dataloader.dataset)
+        for batch, (X, y) in enumerate(dataloader):
+            # Compute prediction and loss
+            pred = self.agent(X)
+            loss = loss_fn(pred, y)
+    
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
     def run(self):
-        raise NotImplementedError
+        loss_fn = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(self.agent.parameters(), lr=self.learning_rate)
+        training_data = CarDataset(data_filepath='scp_data')
+        train_dataloader = DataLoader(training_data, batch_size=self.batch_size, shuffle=True)
+
+        for t in range(self.epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            self.train_loop(train_dataloader, loss_fn, optimizer)
+
     
 
     
