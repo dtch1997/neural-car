7256b41ceb01821200b585f111240c7071d6270b
Daniel Tan <dtch1997@users.noreply.github.com>
2021-05-19 12:08:16 +0800

    Add data generation runner to registry

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -1,7 +1,10 @@
+from src.runners.gen import DataGenerationRunner
 from .train import TrainingRunner
 from .eval import EvaluationRunner
+from .gen import DataGenerationRunner
 
 registry = {
     'train': TrainingRunner,
-    'test': EvaluationRunner
+    'test': EvaluationRunner,
+    'gen': DataGenerationRunner
 }
