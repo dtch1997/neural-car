acdb1be9b373ceba2d322ce8ebe0b6e44d150f7a
Daniel Tan <dtch1997@users.noreply.github.com>
2021-05-19 13:10:00 +0800

    Code for plotting the generated trajectory

---
+++ scratch.py
@@ -0,0 +1,18 @@
+import h5py
+from src.utils.viz import plot_trajectory
+
+with h5py.File('simulation_output.hdf5','r') as file:
+    print(file['simulation_0'].keys())
+    print(file['simulation_0'].attrs.keys())
+
+    sim_0 = file['simulation_0']
+    initial_state = sim_0['state_trajectory'][0]
+    goal_state = sim_0.attrs['goal_state']
+    state_trajectory = sim_0['state_trajectory']
+    obstacle_centers = sim_0.attrs['obstacle_centers']
+    obstacle_radii = sim_0.attrs['obstacle_radii']
+    num_steps = sim_0.attrs['num_steps']
+
+    plot_trajectory(initial_state, goal_state, state_trajectory[:num_steps + 1], 'simulation_output_plot_0.png', 
+            obstacle_centers, obstacle_radii)
+    
